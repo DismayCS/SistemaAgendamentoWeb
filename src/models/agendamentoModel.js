9c86806d9f41800129415acfc36c6d45cdde292f
Juliano Crizel Sigales <Julianocrizelpro@gmail.com>
2025-04-09 23:24:09 -0300

    refactor(models): reorganizar models para seguir boas práticas 🧠

---
+++ src/models/agendamentoModel.js
@@ -0,0 +1,57 @@
+const db = require('../../config/db');
+
+const Agendamento = {
+    // Função para criar um novo agendamento
+    criar: async (cliente_id, servico_id, data_hora) => {
+        try{
+        const query = 'INSERT INTO agendamentos (cliente_id, servico_id, data_hora) VALUES (?, ?, ?)';
+        const [result] = await db.query(query, [cliente_id, servico_id, data_hora]);
+        return { id: result.insertId, cliente_id, servico_id, data_hora };
+        }catch(error){
+            console.error('Erro ao criar agendamentos:', error);
+            throw new Error("Erro ao criar agendamentos");
+        }
+    },
+
+    // Função para listar todos os agendamentos
+    listar: async () => {
+        const sql = `
+            SELECT a.id, a.data_hora, c.nome_completo AS cliente_nome
+            FROM agendamentos a
+            JOIN clientes c ON a.cliente_id = c.id
+        `;
+        try {
+            const [agendamentos] = await db.query(sql);
+            return agendamentos;
+        } catch (error) {
+            console.error('Erro ao listar agendamentos:', error);
+            throw new Error("Erro ao listar agendamentos");
+        }
+    },
+
+
+    editar: async (id, cliente_id, servico_id, data_hora) => {
+        try {
+            const query = `UPDATE agendamentos SET cliente_id = ?, servico_id = ?, data_hora = ? WHERE id = ?`;
+            await db.query(query, [cliente_id, servico_id, data_hora, id]);
+            return { id, cliente_id, servico_id, data_hora };
+        } catch (error) {
+            console.error('Erro ao editar agendamento:', error);
+            throw new Error('Erro ao editar agendamento');
+        }
+    },
+
+    // Função para excluir um agendamento
+    excluir: async (id) => {
+        try {
+            const query = `DELETE FROM agendamentos WHERE id = ?`;
+            await db.query(query, [id]);
+            return { message: 'Agendamento excluído com sucesso!' };
+        } catch (error) {
+            console.error('Erro ao excluir agendamento:', error);
+            throw new Error('Erro ao excluir agendamento');
+        }
+    }
+};
+
+module.exports = Agendamento;
---
+++ src/models/clientModel.js
@@ -0,0 +1,46 @@
+// models/client.js
+
+const db = require('../../config/db'); // Arquivo de configuração de banco de dados
+
+const Client = {
+  // Método para adicionar um novo cliente
+  addClient: async (nome_completo, cpf, data_nascimento, endereco, telefone_fixo, celular, email) => {
+    const query = `
+      INSERT INTO clientes (nome_completo, cpf, data_nascimento, endereco, telefone_fixo, celular, email, created_at)
+      VALUES (?, ?, ?, ?, ?, ?, ?, NOW())
+    `;
+
+    try {
+      const [result] = await db.query(query, [nome_completo, cpf, data_nascimento, endereco, telefone_fixo, celular, email]);
+      return result;
+    } catch (err) {
+      throw err;
+    }
+  },
+
+  // Método para buscar todos os clientes
+  getAllClients: async () => {
+    const query = 'SELECT * FROM clientes';
+    try {
+      const [result] = await db.query(query);
+      return result;
+    } catch (err) {
+      throw err;
+    }
+  },
+
+  // Função para pesquisar clientes com base no termo
+  searchClient: async (termo) => {
+    const query = `SELECT * FROM clientes WHERE nome_completo LIKE ? LIMIT 50`; // Limita para melhorar desempenho
+
+    try {
+      const [result] = await db.query(query, [`%${termo}%`]); // Remove espaços extras
+      return result;
+    } catch (err) {
+      console.error("Erro ao buscar clientes:", err);
+      throw err;
+    }
+  }
+};
+
+module.exports = Client;
